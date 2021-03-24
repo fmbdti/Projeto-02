c8f5aa4cb586d2b537f2213f9b92c81ce8d83e0b
MT-TI <fmbdti@gmail.com>
2021-03-24 16:59:58 -0300

    att

--- idade_anos_cachorro.py
+++ idade_anos_cachorro.py
@@ -2,7 +2,7 @@ idade = int(input("Quantos anos você tem?"))
 
 cachorro = ()
 
-print(f'Se você fosse uma cachorro, você teria {idade * 7} anos!! \n')
+print(f'Se você fosse um cachorro, você teria {idade * 7} anos!! \n')
 
 print(" '0'____' ")
 print("    |||| ")
