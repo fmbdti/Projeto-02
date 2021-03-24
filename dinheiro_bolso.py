ed9a046ae912d07f582a3a98fbce7dd09dcebc4e
MT-TI <fmbdti@gmail.com>
2021-03-23 22:41:55 -0300

    Update dinheiro_bolso.py

--- dinheiro_bolso.py
+++ dinheiro_bolso.py
@@ -1,4 +1,8 @@
-lavagem = int(input("Quantas Lavagem realizadas: "))
-valor = float (input("Valor da Lavagem: "))
-total = lavagem * valor
-print("Total é R$", total)
+l = int(input("Quantas Lavagem realizadas: "))
+v = float(input("Valor da Lavagem: "))
+t = l * v
+
+print (f'Foi realizado %d lavagem no valor de R$ %.2f gerando um total de R$ %.2f' % (l,v,t))
+
+#print (f'Total é R$ {lavagem * valor}' )
+#print("Total é R$", total)
